1f17e60c859f5913303beeb8d284327e3dae4889
atharmohammad <mohd.rule123@gmail.com>
2025-05-02 11:47:45 +0530

    chore: change exposed port to 3005

--- prisma/users.ts
+++ prisma/users.ts
@@ -11,7 +11,12 @@ export const usersToSeed = async (): Promise<
     password: '',
     emailVerifiedAt: new Date(),
     region: SupportedRegion.EU,
-    walletAddress : '7SMfVRrJw75vPzHCQ3ckUCT9igMRre8VHmodTbaVv4R',
+    wallet : {
+      create:{
+        address: '7SMfVRrJw75vPzHCQ3ckUCT9igMRre8VHmodTbaVv4R',
+        lastInteractedAt: new Date()
+      }
+    },
     registry: {
       create: {
         commitment: generateCommitment('TY00002000002'),
